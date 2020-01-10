26f7f26ab46e588038a97b0d0e965d6cda0b8c7d
David Vallner <david@vallner.net>
2020-01-10 19:54:14 +0100

    Initial commit

---
+++ packages/lib/src/foo/bbb/ccc.ts
@@ -0,0 +1,5 @@
+import {ddd} from '@blah/bar';
+
+export function ccc(val: any) {
+	return ddd(val)
+}
---
+++ packages/main/src/index.ts
@@ -0,0 +1,3 @@
+import {yyy} from '@/xxx';
+console.log("50: ", yyy(50));
+console.log("100: ", yyy(100));
